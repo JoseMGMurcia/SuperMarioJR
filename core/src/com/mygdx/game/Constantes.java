3c500c71245200d6ecf4eb5db2ab26599f891213
JoseMGMurcia <49448606+JoseMGMurcia@users.noreply.github.com>
2020-03-15 23:40:22 +0100

    Add files via upload

---
+++ core/src/com/mygdx/game/Constantes.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+public class Constantes {
+
+    public static final float PIXELS_IN_METER = 45f;
+
+    public static final float CAMERA_ADJUST = 100f;
+
+    public static final float ACT_DISTANCE = 20f;
+
+    public static final float HURRY_UP_TIME = 30f;
+
+    public static final float MAX_VELOCITY = 3.5f;
+
+    public static final int SCORE_COIN = 100;
+
+    public static final int SCORE_SETA = 50;
+
+    public static final int SCORE_VIDA = 350;
+
+    public static final int SCORE_SECOND = 10;
+
+
+}
